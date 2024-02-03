bffd1cb2f8fbdc4ac8bafb4bbeafa0efd7d4d003
GTierra2000 <102828602+GTierra2000@users.noreply.github.com>
2024-02-03 17:20:55 -0500

    firtst commit

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <div className="flex-center min-w-full">
+        <img src="/assets/icons/loader.svg"
+        alt="Loader"
+        width={24}
+        height={24}
+        />
+    </div>
+  )
+}
+
+export default Loader
